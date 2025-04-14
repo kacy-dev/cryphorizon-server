5b7babe7f06ef0f8f87523731d0c46886d229b93
Victor Kelechi (Kacy) <kelechukwuvictor414@gmail.com>
2025-04-14 13:43:52 +0100

    Update contactRoute.js

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {
-    sendFormDetails,
+    sendFormDetails
     // getContactFormSubmissions
 } = require('../controllers/contactController');
 
