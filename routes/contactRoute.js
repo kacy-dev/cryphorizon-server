576a0442520780885416821ed7e29bbb6e67f03b
Victor Kelechi (Kacy) <kelechukwuvictor414@gmail.com>
2025-04-14 13:39:40 +0100

    Add files via upload

---
+++ routes/contactRoute.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const {
+    sendFormDetails,
+    // getContactFormSubmissions
+} = require('../controllers/contactController');
+
+
+// const { protectAdmin } = require('../middlewares/authMiddleware'); // ProtectAdmin remains
+
+router.post('/contact', sendFormDetails);
+// router.get('/getDetails', getContactFormSubmissions);
+
+module.exports = router;
