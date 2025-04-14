25fbedaa3b11b0bb3d44af3ab31eac16ae338d7a
Victor Kelechi (Kacy) <kelechukwuvictor414@gmail.com>
2025-04-14 13:42:54 +0100

    Add files via upload

---
+++ models/ContactFormModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const ContactFormModel = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 2,
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+            match: [/^\S+@\S+\.\S+$/, 'Invalid email format'],
+        },
+        mSubject: {
+            type: String,
+            required: true,
+            trim: true,
+            // match: [/^\\+?[0-9]{10,15}$/, 'Invalid mobile number format'],
+        },
+        subject: {
+            type: String,
+            required: true,
+            trim: true,
+            maxlength: 500,
+        },
+    },
+    { timestamps: true }
+);
+
+const ContactForm = mongoose.model('Contact', ContactFormModel);
+
+module.exports = ContactForm;
