3adb148a1972aadc71ec4dcebce5f504b9fc4ecd
Victor Kelechi (Kacy) <kelechukwuvictor414@gmail.com>
2025-04-14 15:04:37 +0100

    Update sendOTP.js

--- config/sendOTP.js
+++ config/sendOTP.js
@@ -96,7 +96,7 @@ const sendOTPEmail = (email, otp, name) => {
 const contactFormNotification = (name, email, mSubject, subject) => {
   const mailOptions = {
     from: email,
-    to: process.env.EMAIL_USER,
+    to: email,
     subject: `New Form Submission From - ${name}`,
     html: `
     <head>
