621309e03da7b04a082076fdace20026b7bfb087
Victor Kelechi (Kacy) <kelechukwuvictor414@gmail.com>
2025-04-16 13:22:26 +0100

    Update contactController.js

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -24,7 +24,7 @@ const sendFormDetails = async (req, res) => {
         await newSubmission.save();
 
         // Send notification email to admin
-        await contactFormNotificationEmail(name, email, mobileNumber, subject);
+        await contactFormNotificationEmail(name, email, mSubject, subject);
 
         return res.status(201).json({
             message: "Your message has been sent successfully",
